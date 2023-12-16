40fd616c62526eff656eeabc46114f4bb8e52a04
Troy Moreland <troy@morelands.net>
2023-12-16 09:29:46 -0600

    fix: changed proto file path to relative

--- src/utils/transformation/index.ts
+++ src/utils/transformation/index.ts
@@ -31,7 +31,7 @@ module.exports = class TransformUtils extends Utils {
       const collection = docParts[0]
       const docId = docParts[1]
 
-      console.log('fs', fs.readFileSync('.', 'utf8'))
+      console.log('fs', fs.readdirSync('.', 'utf8'))
       const root = await protobuf.load('./lib/proto/data.proto')
       const DocumentEventData = root.lookupType(
         'google.events.cloud.firestore.v1.DocumentEventData'
@@ -51,7 +51,7 @@ module.exports = class TransformUtils extends Utils {
 
   encodeFirestoreEvent = async (event: TEvent) => {
     try {
-      console.log('fs', fs.readFileSync('.', 'utf8'))
+      console.log('fs', fs.readdirSync('.', 'utf8'))
       const root = await protobuf.load('./lib/proto/data.proto')
       const DocumentEventData = root.lookupType(
         'google.events.cloud.firestore.v1.DocumentEventData'
